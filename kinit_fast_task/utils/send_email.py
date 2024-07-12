307b0650818275392d8dab5485f06dce679aec12
ktianc <2445667550@qq.com>
2024-07-12 23:41:40 +0800

    邮件发送功能模块更新

--- kinit_fast_task/utils/send_email.py
+++ kinit_fast_task/utils/send_email.py
@@ -9,19 +9,25 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
+from email.utils import formataddr
+
 from kinit_fast_task.core import CustomException
 
 
 class EmailSender:
-    def __init__(self, email: str, password: str, smtp_server: str, smtp_port: int) -> None:
+
+    def __init__(self, *, email: str, password: str, smtp_server: str, smtp_port: int, email_name: str = None) -> None:
         """
         初始化配置
-        :param email:
-        :param password:
-        :param smtp_server:
-        :param smtp_port:
+
+        :param email: 发送人邮箱
+        :param password: 发送人邮箱密码
+        :param smtp_server: 发送邮箱服务器
+        :param smtp_port: 发送邮箱服务器端口
+        :param email_name: 发送人名称
         """
         self.email = email
+        self.email_name = email_name
         self.password = password
         self.smtp_server = smtp_server
         self.smtp_port = smtp_port
@@ -49,7 +55,10 @@ class EmailSender:
         :param attachments: 附件
         """
         message = MIMEMultipart()
-        message["From"] = self.email
+        if self.email_name:
+            message["From"] = formataddr(pair=(self.email_name, self.email))
+        else:
+            message["From"] = self.email
         message["To"] = ", ".join(to_emails)
         message["Subject"] = subject
         body = MIMEText(body)
@@ -71,3 +80,16 @@ class EmailSender:
             self.server.quit()
             print("邮件发送失败！错误信息：", e)
             return False
+
+
+if __name__ == '__main__':
+    params = {
+        "email": "<from_email>",
+        "password": "<password>",
+        "smtp_server": "smtp.163.com",
+        "smtp_port": 465,
+        "email_name": "kinit-fast-task"
+    }
+
+    es = EmailSender(**params)
+    es.send_email(to_emails=["<to_email>"], subject="test", body="test")
